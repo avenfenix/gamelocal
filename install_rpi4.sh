d44eca8dffd13f5c0d2f81db89a2f890807b16ea
Juan Parra <thedonjuan008p@gmail.com>
2023-10-14 19:49:21 -0300

    0.0.0.1v

---
+++ install_rpi4.sh
@@ -0,0 +1,17 @@
+# Requisitos
+sudo apt update
+sudo apt install -y golang
+
+# Compilar
+go build -o gamelocal-client client.go
+go build -o gamelocal-server server.go
+
+# Desintalar version anterior
+sudo rm /usr/local/bin/gamelocal-client
+sudo rm /usr/local/bin/gamelocal-server
+
+# Instalar
+mkdir ~/.gamelocal/
+sudo cp gamelocal-client /usr/local/bin/
+sudo cp gamelocal-server /usr/local/bin/
+
--- server.go
+++ server.go
@@ -5,18 +5,55 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-
-	"github.com/gin-gonic/gin"
+	"net/http"
+	// Importa las librerías necesarias
 )
 
+type Usuario struct {
+	Nombre      string `json:"nombre"`
+	Dispositivo string `json:"dispositivo"`
+	Juego       string `json:"juego"`
+	Directorio  string `json:"directorio"`
+}
+
 func main() {
-	fmt.Println("GameLocal version 0.0.1")
-	r := gin.Default()
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
+	http.HandleFunc("/guardar-partida", func(w http.ResponseWriter, r *http.Request) {
+		// Maneja la solicitud para guardar la partida en el servidor
+		// Guarda la partida en una base de datos o un archivo
+		// ...
+
+		fmt.Println("Partida guardada recibida desde el cliente Gamelocal")
 	})
-	r.Run()
+
+	http.ListenAndServe(":8080", nil) // Escucha en el puerto 8080
+
+	var usuariosConectados []Usuario
+
+	http.HandleFunc("/conectar-usuario", func(w http.ResponseWriter, r *http.Request) {
+		var usuario Usuario
+		err := json.NewDecoder(r.Body).Decode(&usuario)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Verifica si el usuario y el juego son válidos (por ejemplo, GTA San Andreas)
+		// Asocia el juego con el directorio del usuario
+		// ...
+
+		usuariosConectados = append(usuariosConectados, usuario)
+		fmt.Println("Usuario conectado:", usuario.Nombre)
+	})
+
+	http.HandleFunc("/verificar-archivos", func(w http.ResponseWriter, r *http.Request) {
+		// Recibe la solicitud de verificación de archivos del juego
+		// Verifica la integridad de los archivos en el directorio del usuario
+		// ...
+
+		// Devuelve una respuesta al cliente (indicando si los archivos están actualizados o no)
+		// ...
+	})
+
 }
