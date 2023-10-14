238eeca37b75ba8cfa08d9149d9cd5723b883572
Juan Parra <thedonjuan008p@gmail.com>
2023-10-14 20:08:42 -0300

    0.0.0.1v

--- install_rpi4.sh
+++ install_rpi4.sh
@@ -2,10 +2,6 @@
 sudo apt update
 sudo apt install -y golang
 
-# Remover compilacion anterior
-rm gamelocal-client
-rm gamelocal-server
-
 # Compilar
 go build -o gamelocal-client client.go
 go build -o gamelocal-server server.go
@@ -16,6 +12,6 @@ sudo rm /usr/local/bin/gamelocal-server
 
 # Instalar
 mkdir ~/.gamelocal/
-sudo cp gamelocal-client /usr/local/bin/
-sudo cp gamelocal-server /usr/local/bin/
+sudo mv gamelocal-client /usr/local/bin/
+sudo mv gamelocal-server /usr/local/bin/
 
